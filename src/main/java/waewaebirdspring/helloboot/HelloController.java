94188cd321c412e0dd1aa60202350c750db650ed
jzangeva <jzangeva@auto-focus.co.kr>
2024-07-26 16:46:00 +0900

    ~ 21 (S3 Hello 컨트롤러 매핑과 바인딩)

--- src/main/java/waewaebirdspring/helloboot/HelloController.java
+++ src/main/java/waewaebirdspring/helloboot/HelloController.java
@@ -1,14 +1,7 @@
 package waewaebirdspring.helloboot;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-@RestController
 public class HelloController {
-
-    @GetMapping("/hello")
     public String hello(String name) {
         return "hello " + name;
     }
-
 }
