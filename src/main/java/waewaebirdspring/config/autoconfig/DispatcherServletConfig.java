54b2251619f9379ab7f65d3cf7b7acb37f3671cf
waewaebird <jzangeva@gmail.com>
2024-10-22 23:04:02 +0900

    ~ S7

--- src/main/java/waewaebirdspring/config/autoconfig/DispatcherServletConfig.java
+++ src/main/java/waewaebirdspring/config/autoconfig/DispatcherServletConfig.java
@@ -3,8 +3,9 @@ package waewaebirdspring.config.autoconfig;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.DispatcherServlet;
+import waewaebirdspring.config.MyAutoConfiguration;
 
-@Configuration
+@MyAutoConfiguration
 public class DispatcherServletConfig {
     @Bean
     public DispatcherServlet dispatcherServlet() {
