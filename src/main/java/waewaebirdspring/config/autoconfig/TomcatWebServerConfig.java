c66bd0f26be22941e16ef88b8db1575b488f59a3
jzangeva <jzangeva@auto-focus.co.kr>
2024-10-23 14:42:29 +0900

    ~ S8 @Conditional과 Condition

--- src/main/java/waewaebirdspring/config/autoconfig/TomcatWebServerConfig.java
+++ src/main/java/waewaebirdspring/config/autoconfig/TomcatWebServerConfig.java
@@ -2,14 +2,21 @@ package waewaebirdspring.config.autoconfig;
 
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.*;
+import org.springframework.core.type.AnnotatedTypeMetadata;
 import waewaebirdspring.config.MyAutoConfiguration;
 
 @MyAutoConfiguration
+@Conditional(TomcatWebServerConfig.TomcatCondition.class)
 public class TomcatWebServerConfig {
-    @Bean
+    @Bean("tomcatWebServerFactory")
     public ServletWebServerFactory servletWebServerFactory() {
         return new TomcatServletWebServerFactory();
+    }
+
+    static class TomcatCondition implements Condition {
+        public boolean matches(ConditionContext context, AnnotatedTypeMetadata metadata) {
+            return true;
+        }
     }
 }
